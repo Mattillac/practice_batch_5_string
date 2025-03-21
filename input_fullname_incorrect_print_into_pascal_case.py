a3f84fd8aae51d631df7ae4d4378156ee79aad0a
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 03:54:57 +0800

    here is my final code for program 9 as you can see i used the title() str manipulation but i added a modification the .replace() i replaced the space provided by the proper casing of title() so i replaced it with no space making it into a PascalCase

--- input_fullname_incorrect_print_into_pascal_case.py
+++ input_fullname_incorrect_print_into_pascal_case.py
@@ -1,4 +1,7 @@
 #Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 #input name
+name = str(input("Enter your name: "))
 #imagine tObias fOXtROT
-#print tobias_foxtrot
+pascal_cased = name.title().replace(" ","")
+#print TobiasFoxtrot
+print("Your name in Pascal Case:",pascal_cased)
