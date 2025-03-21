539b46be7f365e261e1abbc4184f16ffe58cffae
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 02:34:17 +0800

    as you see this is my final product as i did name.upper to manipulating it into an uppercase text

--- print_fullname_to_uppercase.py
+++ print_fullname_to_uppercase.py
@@ -1,4 +1,7 @@
 #Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
-#input name
-#imagine garen espanto  
+#input name 
+name = str(input("Enter your name: "))
+#imagine garen espanto 
+uppercase_name = name.upper()
 #print GAREN ESPANTO
+print("Your GLORIOUS name in uppercase:",uppercase_name)
