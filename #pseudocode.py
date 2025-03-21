e9c8dfe721f5dbbe4e2b5a15a6eedf7a80ba8405
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 01:58:16 +0800

    i removed pseudocode

--- #pseudocode.py
+++ #pseudocode.py
@@ -1,4 +1,3 @@
-#pseudocode
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning
 #input name with spaces
 name = str(input("Enter your name with space: "))
