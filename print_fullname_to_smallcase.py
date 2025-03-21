970c2807300cc568350fe423cecdbc1d9212c49f
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 02:43:29 +0800

    now here is the final code of my program 4 it is same as the last program but it uses .lower isntead of .upper

--- print_fullname_to_smallcase.py
+++ print_fullname_to_smallcase.py
@@ -1,4 +1,7 @@
 #Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
-#input name
+#input name 
+name = str(input("Enter your name: "))
 #imagine Darius Hilario
+lower_cased = name.lower()
 #print darius hilario
+print("Your unglorious name in lowercase:",lower_cased)
