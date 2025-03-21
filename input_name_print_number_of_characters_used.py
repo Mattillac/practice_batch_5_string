33427737db4f0d5222242777ffc256d3682fa3b6
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 03:42:48 +0800

    here is my final code for program 8 i used the len() since it counts the characters every inserted input in the str

--- input_name_print_number_of_characters_used.py
+++ input_name_print_number_of_characters_used.py
@@ -1,5 +1,8 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 #input name
+name = str(input("Enter your name: "))
 #imagine Garen Espanto
 #counts how many characters are in the input
+characters = len(name)
 #print number of characters
+print("The number of characters present in the input:",characters)
