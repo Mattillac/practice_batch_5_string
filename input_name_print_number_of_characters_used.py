d09e805fdb9238b87f46b544bc9437646dc643f1
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 03:33:21 +0800

    here is my pseudocode for program 8

---
+++ input_name_print_number_of_characters_used.py
@@ -0,0 +1,5 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+#input name
+#imagine Garen Espanto
+#counts how many characters are in the input
+#print number of characters
