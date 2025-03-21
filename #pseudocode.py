19cc3f8d838a09550d5b278668abd7178fba8402
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 01:44:58 +0800

    here is my pesudocode for my program 1

---
+++ #pseudocode.py
@@ -0,0 +1,5 @@
+#pseudocode
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning
+#input name with spaces
+#remove spaces
+#print name without spaces 
