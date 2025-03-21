cd381ea5aa7029a687ac2b25e1474c0394cc8bf3
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 02:56:15 +0800

    jnow here is my final result using title() in order to make the proper casing plus, i forgot to save and commmit my pseudo and instead after i created my pseudo i atuomatically added the code so that is why it looks fast

--- print_proper_casing.py
+++ print_proper_casing.py
@@ -1,4 +1,7 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
-#input name     
+#input name
+name = str(input("Enter your name: "))
 #imagine tObias fOXtROT
+proper_casing = name.title()
 #print Tobias Foxtrot
+print("Your name in proper casing:",proper_casing)
