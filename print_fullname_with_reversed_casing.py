d1e44a1e5bf14eedf3ed1b9f81b57735109e428f
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 03:02:55 +0800

    now this is my final product of program 6 using swapcase() making it swap the caps and lower vice versa

--- print_fullname_with_reversed_casing.py
+++ print_fullname_with_reversed_casing.py
@@ -1,4 +1,7 @@
 #Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 #input name
+name = str(input("Enter your name: "))
 #imagine tObias fOXtROT
+reversed_cased = name.swapcase()
 #print tOBIAS fOXTROT
+print("Your name in esrever gnisac:",reversed_cased)
