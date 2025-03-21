23994b37efc7a267b7c481800bdad123b8e65fe7
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 01:57:06 +0800

    here is the final results using str and lstrip

--- #pseudocode.py
+++ #pseudocode.py
@@ -1,5 +1,8 @@
 #pseudocode
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning
 #input name with spaces
+name = str(input("Enter your name with space: "))
 #remove spaces
+cutted_name = name.lstrip()
 #print name without spaces 
+print(cutted_name)
