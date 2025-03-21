82fda7ce06665d5e6b4ecb8b87da0d47bc0995dc
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 03:14:56 +0800

    here is my pseudocode for program 7

---
+++ input_statement_print_numbers_of_words_used.py
@@ -0,0 +1,5 @@
+#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+#input statement
+#imagine "Hi i like eating cookies, but sometimes i eat cake"
+#expected output: 11
+#print number of words used in the statement
