38bd8dc2a7d14082f1f5789fe7e25a29732b6c83
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 02:54:16 +0800

    here is my pseudocode for program 5

---
+++ print_proper_casing.py
@@ -0,0 +1,4 @@
+#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+#input name     
+#imagine tObias fOXtROT
+#print Tobias Foxtrot
