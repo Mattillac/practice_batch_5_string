eca6ce3c9ffbea9f906e92526ca7e8d29be24e43
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 03:23:18 +0800

    now here is the final result of my program 7 i used .split() and len(). I used split() in order to breakdown the words present in the input, while len() keep track of how many words are breakdowned by split()

--- input_statement_print_numbers_of_words_used.py
+++ input_statement_print_numbers_of_words_used.py
@@ -1,5 +1,9 @@
 #Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 #input statement
+your_statement = str(input("Enter your statement: "))
 #imagine "Hi i like eating cookies, but sometimes i eat cake"
+words_said = your_statement.split()
 #expected output: 11
+word_count = len(words_said)
 #print number of words used in the statement
+print("Words used:",word_count)
