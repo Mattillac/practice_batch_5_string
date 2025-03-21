8db824fda314458529c234ac08151a50f815945a
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 02:25:00 +0800

    now here is the final product using zfill inorder to fill it with zeroes and i add a limit up to 6 as you can see

--- print_6_digits_add_zeroes_at_beginning.py
+++ print_6_digits_add_zeroes_at_beginning.py
@@ -1,4 +1,7 @@
 #Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
-#input number
+#input number   
+number = input("Enter a number: ")
 #imagine inputted number is 123
-#must print 000123
+formed_number = number.zfill(6)
+#must print 000123  
+print("Your entered number in a 6 digits form:",formed_number)
