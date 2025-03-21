ffdc7d93468a0c4fce7acca0e38f853e2411c756
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-22 04:00:54 +0800

    now here is the final product of the last program as you can see i used the same code with the last one. I used .lower() to make all the texts into small case since snake_case uses small casing only and i replace the space with underscores as you can see on the .replace(" ","_")

--- input_fullname_incorrect_print_into_snake_case.py
+++ input_fullname_incorrect_print_into_snake_case.py
@@ -1,4 +1,7 @@
 #Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 #input name
+name = str(input("Enter your name: "))
 #iamgine KiNg koNg
+snake_cased = name.lower().replace(" ","_")
 #prints king_kong
+print("Your name in snake case:",snake_cased)
